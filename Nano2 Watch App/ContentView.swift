dd23029ff489163ca7c1cc9d45366423dba9e15e
Roland Ramadan <129726986+RolandRamadan@users.noreply.github.com>
2024-05-16 09:24:05 +0700

    Initial Commit

---
+++ Nano2 Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nano2 Watch App
+//
+//  Created by Roland Ramadan on 16/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nano2 Watch App/Nano2App.swift
@@ -0,0 +1,17 @@
+//
+//  Nano2App.swift
+//  Nano2 Watch App
+//
+//  Created by Roland Ramadan on 16/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Nano2_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
